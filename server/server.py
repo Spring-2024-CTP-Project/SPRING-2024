84e3c661b7bc90ce67cbb8c4a3e98033553babc3
Joseph <inumoj@gmail.com>
2024-04-04 16:13:22 -0400

    Tried to finish setup with flask and react encountered issue with fetching json data

--- server/server.py
+++ server/server.py
@@ -1,2 +1,12 @@
+from flask import Flask
 
 
+app = Flask(__name__)
+
+#Characters api route example
+@app.route("/characters")
+def characters():
+    return {"characters": ["character1","character2","character3"] }
+
+if __name__=="__main__":
+    app.run(debug=True)
