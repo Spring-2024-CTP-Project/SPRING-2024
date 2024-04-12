264e50d5ffb012180e4d435a25bb4b0183d46ec3
hninl23 <h.lwin0440@gmail.com>
2024-04-12 16:14:07 -0400

    connect to atlas

--- server/server.py
+++ server/server.py
@@ -1,19 +1,26 @@
 from flask import Flask, render_template, jsonify
 from pymongo import MongoClient
 from flask_pymongo import PyMongo
+from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/playersDB"
+app.config["MONGO_URI"] = os.getenv("DB_URI")
+uri = os.getenv("DB_URI")
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 mongo = PyMongo(app)
 
-# mongo.insertOne({
-#       "Name": "Dinorah",
-#       "Race": "Human",
-#       "Class": "Wizard",
-#       "Weapons": ["Staff", "Spellbook"],
-#       "Background": "Scholar"
-#     })
-db_operations = mongo.db.player
+
+db_operations = client.db.player_info
 @app.route("/")
 def home_page():
     users = db_operations.find()
@@ -23,7 +30,7 @@ def home_page():
 
 @app.route('/mongo', methods=['GET'])
 def insert_all_docs():
-  db_operations.insert_one({
+  client.db.player_info.insert_one({
       "Name": "Anthony",
       "Race": "Human",
       "Class": "Wizard",
@@ -31,10 +38,7 @@ def insert_all_docs():
       "Background": "Scholar"
     })
   return "Inserted"
-#Characters api route example
-# @app.route("/characters")
-# def characters():
-#     return {"characters": ["character1","character2","character3"] }
+
 
 if __name__=="__main__":
     app.run(debug=True)
